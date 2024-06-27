c47fce3f26b5cdecfc104bb0601e19a1d2504da5
Vedran <carobnjak.best@gmail.com>
2024-06-27 22:07:06 +0200

    mass saving throws done and rewritten

--- main.py
+++ main.py
@@ -61,6 +61,8 @@ RelPosSettings = RelativePositionTracker()
 RelPosMonsters = RelativePositionTracker()
 RelPosTargets = RelativePositionTracker()
 RelPosMassroll = RelativePositionTracker()
+RelPosRandGen = RelativePositionTracker()
+
 class PlayerStats():
     def __init__(self):
         #TODO: Make a setting to pick between PP and rolling percep for mass percep check
@@ -240,7 +242,7 @@ def CreateMonster() -> None:
     GSM.Monster_to_hit_int.set(6)
     monster_to_hit_entry = tk.Entry(GSM.Monsters_frame, borderwidth=2, textvariable=GSM.Monster_to_hit_int, width=3)
     monster_to_hit_entry.place(x=RelPosMonsters.increase("x", 93), y=RelPosMonsters.same("y"))
-    #TODO: add tenacity, reroll 1s and 2s, brutal critical etc, saving throw on hit
+    #TODO: add savaga attacker, reroll 1s and 2s, brutal critical etc, saving throw on hit, extra damage on 1st hit per turn
     'Dmg 1'
     monster_dmg1_text_label = tk.Label(GSM.Monsters_frame, text="Damage type 1:")
     monster_dmg1_text_label.place(x=RelPosMonsters.reset("x"), y=RelPosMonsters.increase("y", 25))
@@ -348,8 +350,10 @@ def MassSavingThrow() -> None:
                 roll = max(RollDice("d20"), RollDice("d20"), RollDice("d20"))
             elif rolltype == "Super Disadvantage":
                 roll = min(RollDice("d20"), RollDice("d20"), RollDice("d20"))
-            roll = roll + GSM.Mass_save_mod_int.get()
-            if (roll) >= (GSM.Mass_save_DC_int.get()):
+            roll_total = roll + GSM.Mass_save_mod_int.get()
+            if roll == 1 and GSM.Nat1_always_miss_bool.get():
+                pass #nat 1 always fails with rule
+            elif (roll_total) >= (GSM.Mass_save_DC_int.get()):
                 passes += 1
             rolls.append(roll)
 
@@ -392,15 +396,35 @@ def RollDice(die_type: str) -> int:
         die_max = 100
     return random.randint(1, die_max)
 
-fumble_table = ["Get distracted: Disadvantage on next attack", "Overextended Swing: Next attack on you have advantage", "Hit yourself for half dmg",
-                "Hit ally for half dmg", "String Snap: If using a bow or crossbow", "Fall prone", "Ranged attack/spell: Goes in completely random direction and hits something",
-                "Drop your weapon", "Misjudged Distance: enemy within 5ft can attempt a free grapple check", "Twisted Ankle: half move speed next turn",
-                "Overexertion: Lose bonus action this turn", "Self doubt: Lose reaction this turn", "Ice spells: Your feet freeze, is rooted until next turn",
-                "Fire spells: Lit yourself on fire, 2 turns take 1d6 dmg or action to put it out", "Ranged weapons: Drop 10 pieces of ammunition, it scatters and needs action to collect",
-                "A random glass item/potion breaks and spills", "Coin pouch rips, you lose 5d8 GP", "If spell: Roll wild magic table", "Loud noise: You cause a very loud noise",
-                "Panic: Become frightened of whatever you just attacked", "Sand/Mud in eyes: Become blinded until your next turn", "Drop formation: Lose 2 AC until next turn",
-                "Instinct movement: You provoke instincts in enemy, they move 10ft immediately", "Nothing happens, lucky day", "You got distracted by a coin on the ground! Add 1GP to your inventory"]
-TargetDmgWidgets = []
+def RandomGenerator():
+    fumble_table = ["Get distracted: Disadvantage on next attack", "Overextended Swing: Next attack on you have advantage", "Hit yourself for half dmg",
+                    "Hit ally for half dmg", "String Snap: If using a bow or crossbow", "Fall prone", "Ranged attack/spell: Goes in completely random direction and hits something",
+                    "Drop your weapon", "Misjudged Distance: enemy within 5ft can attempt a free grapple check", "Twisted Ankle: half move speed next turn",
+                    "Overexertion: Lose bonus action this turn", "Self doubt: Lose reaction this turn", "Ice spells: Your feet freeze, is rooted until next turn",
+                    "Fire spells: Lit yourself on fire, 2 turns take 1d6 dmg or action to put it out", "Ranged weapons: Drop 10 pieces of ammunition, it scatters and needs action to collect",
+                    "A random glass item/potion breaks and spills", "Coin pouch rips, you lose 5d8 GP", "If spell: Roll wild magic table", "Loud noise: You cause a very loud noise",
+                    "Panic: Become frightened of whatever you just attacked", "Sand/Mud in eyes: Become blinded until your next turn", "Drop formation: Lose 2 AC until next turn",
+                    "Instinct movement: You provoke instincts in enemy, they move 10ft immediately", "Nothing happens, lucky day", "You got distracted by a coin on the ground! Add 1GP to your inventory"]
+    random_gen_result_label = None
+    RelPosRandGen.constant_y = 40
+    def RollFumbleButton():
+        nonlocal random_gen_result_label
+        if random_gen_result_label is not None:
+            random_gen_result_label.destroy()
+
+        random_gen_result_label = tk.Label(GSM.Random_generator_frame, text=(f"{random.choice(fumble_table)}"))
+        random_gen_result_label.place(x=RelPosRandGen.reset("x"), y=RelPosRandGen.set("y", 120))
+
+    # Target settings text
+    Target_settings_text_label = tk.Label(GSM.Random_generator_frame, text="Random Generator", font=GSM.Title_font)
+    Target_settings_text_label.place(x=RelPosRandGen.reset("x"), y=RelPosRandGen.reset("y"))
+
+    roll_fumble_button = tk.Button(GSM.Random_generator_frame, text="Roll fumble", state="normal", command=RollFumbleButton,
+                                               padx=9, background="grey")
+    RelPosRandGen.reset("x")
+    roll_fumble_button.place(x=RelPosRandGen.increase("x", 10), y=RelPosRandGen.increase("y", RelPosRandGen.constant_y))
+
+RandomGenerator()
 
 def ROLL() -> None:
     n_dice1 = GSM.Monster_dmg1_n_dice_int.get()
@@ -522,29 +546,12 @@ ROLL_button = tk.Button(GSM.ROLL_frame, text="ROLL", state="normal", command=ROL
                                            padx=9, background="red")
 ROLL_button.grid(row=Row.increase(), column=0, sticky="w", padx=360)
 
-def RollFumbleButton():
-    for widget in TargetDmgWidgets:
-        widget.destroy()
-    TargetDmgWidgets.clear()
-    fumble_label = tk.Label(GSM.Random_generator_frame, text=(f"{random.choice(fumble_table)}"))
-    fumble_label.grid(column=0, padx=360, row=_first_target3_row, sticky="w")
 
-    TargetDmgWidgets.append(fumble_label)
-nat1_button = tk.Button(GSM.Random_generator_frame, text="Roll fumble", state="normal", command=RollFumbleButton,
-                                           padx=9, background="grey")
-nat1_button.grid(row=Row.same(), column=0, sticky="w", padx=530)
-
-Space = tk.Label(GSM.Random_generator_frame, text="").grid(row=Row.increase(), column=0, sticky="w", padx=360)
-#Target settings text
-Target_settings_text_label = tk.Label(GSM.Random_generator_frame, text="Roll results", font=GSM.Title_font)
-Target_settings_text_label.grid(row=Row.increase(), column=0, sticky="w", padx=360)
-
-_first_target3_row = Row.increase()
 
 #TODO: Add a boss section, which tracks boss cooldowns, legendary actions etc
 #TODO: Add automated rolling for random encounter table. Let players import a list, saying chance of each encounter,
 #   which happen per night and which per day, how often to check, and how long PC's travelled and then determine the outcome
 #TODO: Add a random store. Decide "quality" of store (distribution of item rarity), how many items, read an external list, and price range as inputs
-
+#TODO: Add random loot
 
 GSM.Root.mainloop()
