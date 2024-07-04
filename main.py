4a7227fb90bfcc4e73e114117f344212e38c3015
Vedran <carobnjak.best@gmail.com>
2024-07-04 22:17:08 +0200

    Added spellcasters, works like a charm

--- main.py
+++ main.py
@@ -619,12 +619,128 @@ def SpellCasters() -> None:
                 checkbox.destroy()
             GSM.Spell_checkboxes_dict[index] = []
 
-            # Example spells for demonstration, you could customize this per level
-            spells = ["Spell 1", "Spell 2", "Spell 3"]
-            for i, spell in enumerate(spells):
-                cb = tk.Checkbutton(GSM.Spell_caster_frame, text=spell)
-                cb.place(x=RelPosSpellCast.reset("x") + column_offset, y=base_y + 25 * i)
-                GSM.Spell_checkboxes_dict[index].append(cb)
+            if caster_lv >= 1:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 1:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.set("y", 145))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 2:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 3:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 2:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 4:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 5:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 3:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 6:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 7:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 4:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 8:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 9:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 5:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 10:
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.increase("x", 20) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 11:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 6:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 13:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 7:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 15:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 8:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 17:
+                spell_text_label = tk.Label(GSM.Spell_caster_frame, text="Level 9:")
+                spell_text_label.place(x=RelPosSpellCast.reset("x") + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_text_label)
+
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 47) + column_offset, y=RelPosSpellCast.same("y"))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 18: #Specijalni RelPos X i Y jer idu unatrad
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 87) + column_offset, y=RelPosSpellCast.increase("y", -80))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv >= 19: #Specijalni RelPos X i Y jer idu unatrad
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 67) + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+            if caster_lv == 20: #Specijalni RelPos X i Y jer idu unatrad
+                spell_checkbox = tk.Checkbutton(GSM.Spell_caster_frame)
+                spell_checkbox.place(x=RelPosSpellCast.set("x", 67) + column_offset, y=RelPosSpellCast.increase("y", 20))
+                GSM.Spell_checkboxes_dict[index].append(spell_checkbox)
+
+
 
         for i in range(GSM.N_casters_int.get()):
             column_offset = i * 165
@@ -652,7 +768,6 @@ def SpellCasters() -> None:
     n_casters_dropdown = tk.OptionMenu(GSM.Spell_caster_frame, GSM.N_casters_int, *[1, 2, 3, 4, 5], command=CreateSpellCasters)
     n_casters_dropdown.place(x=RelPosSpellCast.increase("x", 135), y=RelPosSpellCast.increase("y", -4))
 
-    # TODO: Add spellcasters, maybe in separate tab
 
 SpellCasters()
 
